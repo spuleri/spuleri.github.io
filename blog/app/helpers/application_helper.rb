4464d70e248666b62afebc18f88cfb62dcce4974
Sergio Puleri <me@sergiopuleri.com>
2017-07-14 13:48:08 -0400

    add markdown rendering support

--- blog/app/helpers/application_helper.rb
+++ blog/app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  class HTMLWithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+  def markdown(content)
+    renderer = HTMLWithPygments.new(hard_wrap: true, filter_html: true)
+		options = {
+			autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true,
+      prettify: true,
+      highlight: true,
+      footnotes:true,
+      quote:true
+		}
+		Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
 end
