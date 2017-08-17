ee9de01bc978aeea12ce247cc89e7c3ed54ad2aa
Sergio Puleri <me@sergiopuleri.com>
2017-08-17 00:14:12 -0400

    Update check_revision task to remove hardcoded branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,8 +54,9 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      branch = fetch(:branch)
+      unless `git rev-parse HEAD` == `git rev-parse origin/"#{branch}"`
+        puts "WARNING: HEAD is not the same as origin/#{branch}"
         puts "Run `git push` to sync changes."
         exit
       end
